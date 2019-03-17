008565a73d130db6981e315b862d62542dbef1f2
Zach Drake <zadrake@yahoo.com>
2019-03-16 21:23:14 -0700

    pseudo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+// four crystal buttons
+// each crystal generates a random number 1-12
+//      hide that number until player clicks
+//      when clicked update player's score counter
+// display random number for winning value 19-120
+// 
